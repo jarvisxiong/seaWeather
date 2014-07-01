0c7473b66a06219fc3299b50a9869ed061e6e912
Administrator <Administrator@USER-20140511KL>
2014-07-01 20:16:16 +0800

    将错误提示信息修改为英文，并且增加多一次获取数据

--- src/com/sea/weather/utils/GrabTask.java
+++ src/com/sea/weather/utils/GrabTask.java
@@ -1,15 +1,23 @@
 package com.sea.weather.utils;
 
+import java.util.Date;
 import java.util.TimerTask;
 
 public class GrabTask extends TimerTask{
 	@Override
 	public void run() {
-		try{
+		try {
 			CacheDate.loadDataSource();
-		}catch(Exception e){
-			System.out.println("执行定时任务出现异常:"+e.getClass().getName());
+		} catch (Exception e) {
+			System.out.println("do GrabTask Exception first:"
+					+ e.getClass().getName() + ",time:" + new Date());
+			try {
+				CacheDate.loadDataSource();
+			} catch (Exception e2) {
+				System.out.println("do GrabTask Exception two:"
+						+ e2.getClass().getName() + ",time:" + new Date());
+			}
+
 		}
 	}
-
 }
