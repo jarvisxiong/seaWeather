46c42b7fc33c4104f03d982aee975418a4175bdf
danjiachuan@163.com <danjiachuan@163.com>
2014-09-19 18:00:41 +0800

    修改更新日志，取消读读的限制

--- WebContent/CacheRssNewsListDate.jsp
+++ WebContent/CacheRssNewsListDate.jsp
@@ -24,9 +24,6 @@
 	if (versionCode < SeaConstant.versionCode) {
 		verstr = (String) Cache.getValue(Cachekey.rsskey_5);
 	}
-	if(versionCode == 9){
-		verstr = (String) Cache.getValue(Cachekey.rsskey_noNews);
-	}
 	if (StringUtils.isBlank(verstr)) {
 		verstr = CacheDate.getRssNewsList();
 	}
--- src/com/sea/weather/utils/UpdateLog.java
+++ src/com/sea/weather/utils/UpdateLog.java
@@ -3,9 +3,9 @@ package com.sea.weather.utils;
 public class UpdateLog {
 
 	public static final String appName = "海洋天气";
-	public static final int versionCode = 7;
-	public static final String versionName = "1.6";
-	public static final String updateUrl = "http://gdown.baidu.com/data/wisegame/3e61ac38e4663dc7/haiyangtianqi_7.apk";
+	public static final int versionCode = 9;
+	public static final String versionName = "1.8";
+	public static final String updateUrl = "http://gdown.baidu.com/data/wisegame/df00a9522889ad79/haiyangtianqi_9.apk";
 	public static final String updateContent="1.增加台风路径图";
 	public static final String title="海洋天气1.8版本发布";
 	
