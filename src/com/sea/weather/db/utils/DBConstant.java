a65798fa9e48a16f401f32a8ac9c6b3e81bf9809
danjiachuan@163.com <danjiachuan@163.com>
2014-11-12 18:19:56 +0800

    数据库驱动的更改

--- src/com/sea/weather/db/utils/DBConstant.java
+++ src/com/sea/weather/db/utils/DBConstant.java
@@ -2,7 +2,20 @@ package com.sea.weather.db.utils;
 
 public class DBConstant {
 
-	    // 驱动程序名
+//	    // 驱动程序名
+//		public static final String DRIVER = "com.mysql.jdbc.Driver";
+//		
+//		// URL指向要访问的数据库名 jdbc:mysql://sqld.duapp.com:4050/MVaMeIidhltQBJokfLdX
+//		public static final String URL = "jdbc:mysql://sqld.duapp.com:4050/MVaMeIidhltQBJokfLdX";
+//		
+//		  // MySQL配置时的用户名 CLm02ATTIo2UIzbvDAu8G6ic
+//		public static final String USER = "CLm02ATTIo2UIzbvDAu8G6ic"; 
+//
+//	    // MySQL配置时的密码 bxtZliZl4lhkKjScNrtZC1UUKuc3QZOU
+//		public static final String PASSWORD = "bxtZliZl4lhkKjScNrtZC1UUKuc3QZOU";
+		
+		
+		 // 驱动程序名
 		public static final String DRIVER = "com.mysql.jdbc.Driver";
 		
 		// URL指向要访问的数据库名 jdbc:mysql://sqld.duapp.com:4050/MVaMeIidhltQBJokfLdX
@@ -13,5 +26,6 @@ public class DBConstant {
 
 	    // MySQL配置时的密码 bxtZliZl4lhkKjScNrtZC1UUKuc3QZOU
 		public static final String PASSWORD = "root";
+		
 	
 }
