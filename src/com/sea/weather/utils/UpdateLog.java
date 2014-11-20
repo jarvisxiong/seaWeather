74ed3bd7a4107ba0250ba9152f71e56cb7e358b3
danjiachuan@163.com <danjiachuan@163.com>
2014-11-20 20:06:46 +0800

    修改更新信息

--- src/com/sea/weather/utils/UpdateLog.java
+++ src/com/sea/weather/utils/UpdateLog.java
@@ -3,10 +3,10 @@ package com.sea.weather.utils;
 public class UpdateLog {
 
 	public static final String appName = "海洋天气";
-	public static final int versionCode = 9;
-	public static final String versionName = "1.8";
-	public static final String updateUrl = "http://gdown.baidu.com/data/wisegame/df00a9522889ad79/haiyangtianqi_9.apk";
-	public static final String updateContent="1.增加台风路径图";
-	public static final String title="海洋天气1.8版本发布";
+	public static final int versionCode = 10;
+	public static final String versionName = "2.0";
+	public static final String updateUrl = "http://gdown.baidu.com/data/wisegame/6f32edb57bf3cb20/haiyangtianqi_10.apk";
+	public static final String updateContent="1、增加潮汐预报功能\n2、增加海流预报功能\n3、修改海洋天气启动图标\n4、增加海区收藏夹功能\n5、增加短信分享台风\n6、优化界面布局\n7、台风图增加缩放功能";
+	public static final String title="2.0版本发布,升级仅需1M";
 	
 }
