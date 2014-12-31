e33e640c04d1b96a1616cd36f7caa3a788e6c6be
danjiachuan@163.com <danjiachuan@163.com>
2014-12-31 18:01:05 +0800

    海事通告的rss地址

---
+++ src/com/sea/weather/utils/MsaRssConstant.java
@@ -0,0 +1,50 @@
+package com.sea.weather.utils;
+
+public class MsaRssConstant {
+
+	public final static String shJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa0302062.xml";
+	
+	public final static String tjJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa0302063.xml";
+	
+	public final static String lnJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa0302064.xml";
+	
+	public final static String hbJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa0302065.xml";
+	
+	public final static String sdJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa0302066.xml";
+	
+	public final static String jsJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa0302067.xml";
+	
+	public final static String zjJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa0302068.xml";
+	
+	public final static String fjJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa0302069.xml";
+	
+	public final static String gdJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020610.xml";
+	
+	public final static String gxJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020611.xml";
+	
+	public final static String hnJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020612.xml";
+	
+	public final static String cjJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020613.xml";
+	
+	public final static String hljJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020614.xml";
+	
+	public final static String szJG =   "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020615.xml";
+	
+	public final static String bjdfJG = "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020616.xml";
+	
+	public final static String shdfJG =   "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020617.xml";
+	
+	public final static String tjdfJG =   "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020618.xml";
+	
+	public final static String cqdfJG =   "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020619.xml";
+	
+	public final static String nmgdfJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020620.xml";
+	
+	public final static String hlbedfJG = "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020621.xml";
+	
+	public final static String sxdfJG  =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020622.xml";
+	
+	public final static String hbdfJG  =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020623.xml";
+	
+	public final static String lndfJG  =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020624.xml";
+}
