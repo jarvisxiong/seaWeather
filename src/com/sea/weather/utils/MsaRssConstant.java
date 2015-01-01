152a91a45b73092544ad9a6a495b9789ae7397cb
huanzhishuiyue <huanzhiyue001@163.com>
2015-01-01 11:51:23 +0800

    提交代码

--- src/com/sea/weather/utils/MsaRssConstant.java
+++ src/com/sea/weather/utils/MsaRssConstant.java
@@ -2,49 +2,50 @@ package com.sea.weather.utils;
 
 public class MsaRssConstant {
 
+	//上海海事局航行警告
 	public final static String shJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa0302062.xml";
-	
+	//天津海事局航行警告 
 	public final static String tjJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa0302063.xml";
-	
+	//辽宁海事局航行警告 
 	public final static String lnJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa0302064.xml";
-	
+	//河北海事局航行警告 
 	public final static String hbJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa0302065.xml";
-	
+	//山东海事局航行警告 
 	public final static String sdJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa0302066.xml";
-	
+	//江苏海事局航行警告
 	public final static String jsJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa0302067.xml";
-	
+	//浙江海事局航行警告 
 	public final static String zjJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa0302068.xml";
-	
+	//福建海事局航行警告 
 	public final static String fjJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa0302069.xml";
-	
+	//广东海事局航行警告 
 	public final static String gdJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020610.xml";
-	
+	//广西海事局航行警告 
 	public final static String gxJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020611.xml";
-	
+	//海南海事局航行警告
 	public final static String hnJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020612.xml";
-	
+	//长江海事局航行警告
 	public final static String cjJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020613.xml";
-	
+	//黑龙江海事局航行警告 
 	public final static String hljJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020614.xml";
-	
+	//深圳海事局航行警告 
 	public final static String szJG =   "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020615.xml";
-	
+	//北京市地方海事局航行警告 
 	public final static String bjdfJG = "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020616.xml";
-	
+	//上海市地方海事局航行警告 
 	public final static String shdfJG =   "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020617.xml";
-	
+	//天津市地方海事局航行警告 
 	public final static String tjdfJG =   "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020618.xml";
-	
+	//重庆市地方海事局航行警告 
 	public final static String cqdfJG =   "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020619.xml";
-	
+	//内蒙古自治区地方海事局航行警告 
 	public final static String nmgdfJG =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020620.xml";
-	
+	//呼伦贝尔地方海事局航行警告
 	public final static String hlbedfJG = "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020621.xml";
-	
+	//山西省地方海事局航行警告 
 	public final static String sxdfJG  =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020622.xml";
-	
+	//河北省地方海事局航行警告
 	public final static String hbdfJG  =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020623.xml";
-	
+	//辽宁省地方海事局航行警告 
 	public final static String lndfJG  =  "http://www.msa.gov.cn/Rss-Msa/Rss2Msa03020624.xml";
 }
