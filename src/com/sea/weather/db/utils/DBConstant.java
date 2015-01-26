bde838eee2516085e427ec63da3c960c7f9e38e8
danjiachuan@163.com <danjiachuan@163.com>
2015-01-26 10:54:45 +0800

    修复潮汐的缺陷

--- src/com/sea/weather/db/utils/DBConstant.java
+++ src/com/sea/weather/db/utils/DBConstant.java
@@ -2,30 +2,30 @@ package com.sea.weather.db.utils;
 
 public class DBConstant {
 
-//	    // 驱动程序名
-//		public static final String DRIVER = "com.mysql.jdbc.Driver";
-//		
-//		// URL指向要访问的数据库名 jdbc:mysql://sqld.duapp.com:4050/MVaMeIidhltQBJokfLdX
-//		public static final String URL = "jdbc:mysql://sqld.duapp.com:4050/MVaMeIidhltQBJokfLdX";
-//		
-//		  // MySQL配置时的用户名 CLm02ATTIo2UIzbvDAu8G6ic
-//		public static final String USER = "CLm02ATTIo2UIzbvDAu8G6ic"; 
-//
-//	    // MySQL配置时的密码 bxtZliZl4lhkKjScNrtZC1UUKuc3QZOU
-//		public static final String PASSWORD = "bxtZliZl4lhkKjScNrtZC1UUKuc3QZOU";
-		
-		
-		 // 驱动程序名
+	    // 驱动程序名
 		public static final String DRIVER = "com.mysql.jdbc.Driver";
 		
 		// URL指向要访问的数据库名 jdbc:mysql://sqld.duapp.com:4050/MVaMeIidhltQBJokfLdX
-		public static final String URL = "jdbc:mysql://127.0.0.1:3306/seaweather";
+		public static final String URL = "jdbc:mysql://sqld.duapp.com:4050/MVaMeIidhltQBJokfLdX";
 		
 		  // MySQL配置时的用户名 CLm02ATTIo2UIzbvDAu8G6ic
-		public static final String USER = "root"; 
+		public static final String USER = "CLm02ATTIo2UIzbvDAu8G6ic"; 
 
 	    // MySQL配置时的密码 bxtZliZl4lhkKjScNrtZC1UUKuc3QZOU
-		public static final String PASSWORD = "root";
+		public static final String PASSWORD = "bxtZliZl4lhkKjScNrtZC1UUKuc3QZOU";
+		
+		
+//		 // 驱动程序名
+//		public static final String DRIVER = "com.mysql.jdbc.Driver";
+//		
+//		// URL指向要访问的数据库名 jdbc:mysql://sqld.duapp.com:4050/MVaMeIidhltQBJokfLdX
+//		public static final String URL = "jdbc:mysql://127.0.0.1:3306/seaweather";
+//		
+//		  // MySQL配置时的用户名 CLm02ATTIo2UIzbvDAu8G6ic
+//		public static final String USER = "root"; 
+//
+//	    // MySQL配置时的密码 bxtZliZl4lhkKjScNrtZC1UUKuc3QZOU
+//		public static final String PASSWORD = "root";
 		
 	
 }
