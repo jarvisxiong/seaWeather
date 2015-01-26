7a25b10b7b4b89aa0e5e77ef80b09a56c1a186ef
danjiachuan@163.com <danjiachuan@163.com>
2015-01-26 10:38:07 +0800

    增加版本更新信息

--- src/com/sea/weather/utils/UpdateLog.java
+++ src/com/sea/weather/utils/UpdateLog.java
@@ -3,10 +3,10 @@ package com.sea.weather.utils;
 public class UpdateLog {
 
 	public static final String appName = "海洋天气";
-	public static final int versionCode = 10;
-	public static final String versionName = "2.0";
-	public static final String updateUrl = "http://gdown.baidu.com/data/wisegame/6f32edb57bf3cb20/haiyangtianqi_10.apk";
-	public static final String updateContent="升级仅需1M\n1、增加潮汐预报功能\n2、增加海流预报功能\n3、修改海洋天气启动图标\n4、增加海区收藏夹功能\n5、增加短信分享台风\n6、优化界面布局\n7、台风图增加缩放功能";
-	public static final String title="2.0版本发布,升级仅需1M";
+	public static final int versionCode = 11;
+	public static final String versionName = "2.1";
+	public static final String updateUrl = "http://gdown.baidu.com/data/wisegame/fc5d7f4f6b8b02ef/haiyangtianqi_11.apk";
+	public static final String updateContent="升级仅需1.5M\n1、新增海上大风预警功能\n2、新增天气公报（中国海区天气概述）功能\n3、增加航行警告和航行通告功能（如航标变动通知等）\n4、增加环境预报（包括浪高，涌高，水温，流速，流向）\n5、潮汐增加记住上一次选择功能\n6、中国沿岸，近海，远海，环境预报增加海区分布地图\n7、修复读读不能打开的缺陷";
+	public static final String title="2.1版本发布,升级仅需1.5M";
 	
 }
